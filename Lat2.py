49dfa1268fa7b030089d2ed067887f15dd4f462c
nabilahap <nabilaananda1325@gmail.com>
2021-11-04 23:59:36 +0700

    Lab2

---
+++ Lat2.py
@@ -0,0 +1,8 @@
+print('Program mengurutkan data')
+
+pertama = int(input("Bilangan pertama: "))
+kedua = int(input("Bilangan kedua: "))
+ketiga = int(input("Bilangan ketiga: "))
+
+print(f'urutan: {pertama}, {kedua}, {ketiga}')
+
