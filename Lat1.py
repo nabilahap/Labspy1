d87492f5d4a0e99df797e3ba2e7687542472a943
nabilahap <nabilaananda1325@gmail.com>
2021-11-04 23:58:58 +0700

    Lab2

---
+++ Lat1.py
@@ -0,0 +1,10 @@
+print("Program mencari bilangan terbesar") 
+
+a = int(input("Masukan nilai a: "))
+b = int(input("Masukan nilai b: "))
+
+if a > b:
+    print ("A yang terbesar")
+elif b > a:
+    print ("B yang terbesar")
+
